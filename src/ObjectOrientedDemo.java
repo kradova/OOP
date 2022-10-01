a46ed70abac1913aecf2d20a9aaa28104303db98
kradova <ksenia.radova@gmail.com>
2022-10-01 17:35:57 +0300

    + static fields&methods

--- src/ObjectOrientedDemo.java
+++ src/ObjectOrientedDemo.java
@@ -1,38 +1,47 @@
 public class ObjectOrientedDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-//        Staff a = new Staff();
-//        a.setHoursWorked(10);
+//        System.out.println("Hello World!");
 //
-//        System.out.println(a.getHoursWorked());
+////        Staff a = new Staff();
+////        a.setHoursWorked(10);
+////
+////        System.out.println(a.getHoursWorked());
+//
+//
+//        Cat cat_1 = new Cat();
+//        System.out.println(cat_1.getName());
+//
+//        cat_1.setName("Archi");
+//        System.out.println(cat_1.getName());
+//
+//        Cat cat_2 = new Cat("Leva", 7);
+//        System.out.println("Name: " + cat_2.getName() +
+//                "; Age: " + cat_2.getAge());
+//
+//        Staff staff1 = new Staff("Peter");
+//        staff1.setHoursWorked(160);
+//        int pay = staff1.calculatePay(1000, 400);
+//        System.out.println("Pay = " + pay);
+//
+//        Staff staff2 = new Staff("Jane", "Lee");
+//        staff2.setHoursWorked(160);
+//        pay = staff2.calculatePay();
+//        System.out.println("Pay = " + pay);
+//
+//        System.out.println("\n\nUpdating Jane's Hours Worked to - 10");
+//        staff2.setHoursWorked(-10);
+//        System.out.println("\nHours Worked = " + staff2.getHoursWorked());
+//        pay = staff2.calculatePay();
+//        System.out.println("Pay = " + pay);
+        StaticDemo sd = new StaticDemo();
+        System.out.println(sd.message);
+        sd.displayMessage();
+
+        System.out.println(StaticDemo.greetings);
+        StaticDemo.displayGreetings();
 
 
-        Cat cat_1 = new Cat();
-        System.out.println(cat_1.getName());
-
-        cat_1.setName("Archi");
-        System.out.println(cat_1.getName());
-
-        Cat cat_2 = new Cat("Leva", 7);
-        System.out.println("Name: " + cat_2.getName() +
-                "; Age: " + cat_2.getAge());
-
-        Staff staff1 = new Staff("Peter");
-        staff1.setHoursWorked(160);
-        int pay = staff1.calculatePay(1000, 400);
-        System.out.println("Pay = " + pay);
-
-        Staff staff2 = new Staff("Jane", "Lee");
-        staff2.setHoursWorked(160);
-        pay = staff2.calculatePay();
-        System.out.println("Pay = " + pay);
-
-        System.out.println("\n\nUpdating Jane's Hours Worked to - 10");
-        staff2.setHoursWorked(-10);
-        System.out.println("\nHours Worked = " + staff2.getHoursWorked());
-        pay = staff2.calculatePay();
-        System.out.println("Pay = " + pay);
     }// main
-}
+}//ObjectOrientedDemo
+
 
---
+++ src/StaticDemo.java
@@ -0,0 +1,12 @@
+public class StaticDemo {
+    public String message = "Hello World";
+    public void displayMessage(){
+        System.out.println("Message = " + message);
+    }
+
+    public static String greetings = "Good morning";
+    public static void displayGreetings(){
+        System.out.println("Greeting = " + greetings);
+    }
+}//class StaticDemo
+
