bcdf0142f4bf3cab332e2d2e3a81a940c1d58bbb
kradova <ksenia.radova@gmail.com>
2022-10-08 16:16:48 +0300

    + using overrided methods

--- src/inheritance_demo/InheritanceDemo.java
+++ src/inheritance_demo/InheritanceDemo.java
@@ -2,7 +2,16 @@ package inheritance_demo;
 
 public class InheritanceDemo {
     public static void main(String[] args) {
-        NormalMember myChildMember = new NormalMember("James", 1, 2010);
+        NormalMember mem1 = new NormalMember("James", 1, 2010);
         VIPMember mem2 = new VIPMember("Andy", 2, 2011);
+
+        mem2.calculateAnnualFee();
+        mem2.displayMemInfo();
+
+        mem1.setDiscount();
+        mem1.calculateAnnualFee();
+        mem1.displayMemInfo();
+
+
     }
 }
