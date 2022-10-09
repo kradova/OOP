5ed0af20e9abe875e9c19325fd26898542670ba1
kradova <ksenia.radova@gmail.com>
2022-10-09 17:19:30 +0300

    + using implemented method

--- src/interface_demo/InterfaceDemo.java
+++ src/interface_demo/InterfaceDemo.java
@@ -4,5 +4,7 @@ public class InterfaceDemo {
     public static void main(String[] args) {
         MyClass a = new MyClass();
         a.someMethod();
+
+        System.out.println("The value of the constant is " + MyInterface.myInt);
     }
 }
