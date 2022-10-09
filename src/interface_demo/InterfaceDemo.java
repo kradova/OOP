74941ff0297bfebaa35e4a9f8b53a9aef8d4e65a
kradova <ksenia.radova@gmail.com>
2022-10-09 17:11:48 +0300

    + implements myinterface

--- src/interface_demo/InterfaceDemo.java
+++ src/interface_demo/InterfaceDemo.java
@@ -1,2 +1,8 @@
-package interface_demo;public class InterfaceDemo {
+package interface_demo;
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        MyClass a = new MyClass();
+        a.someMethod();
+    }
 }
--- src/interface_demo/MyClass.java
+++ src/interface_demo/MyClass.java
@@ -1,2 +1,9 @@
-package interface_demo;public class MyClass {
+package interface_demo;
+
+public class MyClass implements MyInterface{
+
+    @Override
+    public void someMethod() {
+        System.out.println("This is a method implemented in MyClass");
+    }
 }
