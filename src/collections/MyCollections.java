6e0add1e98903be1d312a1613217d7825a47765b
kradova <ksenia.radova@gmail.com>
2022-10-13 23:57:39 +0300

    + Collections

---
+++ src/collections/MyCollections.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyCollections {
+    public static void main(String[] args) {
+        ArrayList<Integer> userAgeList = new ArrayList<>();
+        ArrayList<String> userNameList = new ArrayList<>();
+        List<String> userNameList2 = new ArrayList<>();
+
+    }//main
+}//MyCollections
