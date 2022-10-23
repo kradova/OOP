d14096ca593342622b362429d5dffa0e6b1a46eb
kradova <ksenia.radova@gmail.com>
2022-10-23 16:12:53 +0300

    + modern approach reading from the file

--- src/file_handling/FileHandlingDemo.java
+++ src/file_handling/FileHandlingDemo.java
@@ -6,27 +6,36 @@ public class FileHandlingDemo {
     public static void main(String[] args) {
 
         String line;
-        BufferedReader reader = null;
+//        BufferedReader reader = null;
 
-        try {
-            reader = new BufferedReader(new FileReader("myFile.txt"));
+//        try {
+//            reader = new BufferedReader(new FileReader("myFile.txt"));
+//            line = reader.readLine();
+//            while (line != null) {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        } catch (IOException e) {
+//            System.out.println("================================");
+//            System.out.println(e.getMessage());
+//        } finally {
+//            try {
+//                if (reader != null) {
+//                    reader.close();
+//                }
+//            } catch (IOException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
             line = reader.readLine();
             while (line != null) {
                 System.out.println(line);
                 line = reader.readLine();
             }
         } catch (IOException e) {
-            System.out.println("================================");
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
         }
-
     }//main
 }//class FileHandlingDemo
