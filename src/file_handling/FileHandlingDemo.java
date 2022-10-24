15120ce5601e3c415b8a64197f2d331b77b4afad
kradova <ksenia.radova@gmail.com>
2022-10-24 23:13:12 +0300

    + write to the file(append)

--- src/file_handling/FileHandlingDemo.java
+++ src/file_handling/FileHandlingDemo.java
@@ -28,12 +28,19 @@ public class FileHandlingDemo {
 //            }
 //        }
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
-            line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
+//        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
+//            line = reader.readLine();
+//            while (line != null) {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+        String text = "Hello World ";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("myFile2.txt", true))) {
+            writer.write(text);
+            writer.newLine();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
