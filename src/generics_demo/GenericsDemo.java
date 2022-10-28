d7331cb39ecd867536fd23f842474a65532cc82a
kradova <ksenia.radova@gmail.com>
2022-10-28 22:52:37 +0300

    + limited type

--- src/generics_demo/GenericsDemo.java
+++ src/generics_demo/GenericsDemo.java
@@ -3,12 +3,13 @@ package generics_demo;
 public class GenericsDemo {
     public static void main(String[] args) {
         MyGenericsClass g = new MyGenericsClass();
-        g.setMyVar("Java");
+//        g.setMyVar("Java");
+        g.setMyVar(5);
         g.printValue();
     }//main
 }
 
-class MyGenericsClass<T> {
+class MyGenericsClass<T extends Number> {
     private T myVar;
 
     void setMyVar(T i) {
